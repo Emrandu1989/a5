f49a9e48b7b4d225df48d6e5a8b40cf79351f1ce
Emrandu1989 <emrandu1989@gmail.com>
2024-02-18 21:02:43 +0600

    next button enable

--- js/index.js
+++ js/index.js
@@ -1,119 +1,87 @@
-
 let selectedSeatNo = 0;
 let seatLeftCount = 40;
 let totalTicketPrice = 0;
 let grandTotal = 0;
 let ticketLimit = 4;
 
-
 function buttonClickHandler(event) {
-     if(selectedSeatNo < ticketLimit){
-        console.log('btn-clicked');
+  if (selectedSeatNo < ticketLimit) {
+    console.log("btn-clicked");
     let text = this.innerText;
-    
-    const seatContainer = document.getElementById('seat-no');
-    const p = document.createElement('p');
+
+    const seatContainer = document.getElementById("seat-no");
+    const p = document.createElement("p");
     p.innerText = text;
     seatContainer.appendChild(p);
 
-    this.style.backgroundColor = '#1DD100';
+    this.style.backgroundColor = "#1DD100";
     selectedSeatNo++;
-    seatLeftCount--; 
+    seatLeftCount--;
     totalTicketPrice += 550;
-    grandTotal +=550;
-   
-     
-    const seatCount = document.getElementById('selectedSeat-no');
+    grandTotal += 550;
+
+    const seatCount = document.getElementById("selectedSeat-no");
     seatCount.innerText = selectedSeatNo;
-    const seatLeft = document.getElementById('seat-left');
+    const seatLeft = document.getElementById("seat-left");
     seatLeft.innerText = seatLeftCount;
 
-    const totalPrice = document.getElementById('total-price');
+    const totalPrice = document.getElementById("total-price");
     totalPrice.innerText = totalTicketPrice;
 
-   
-
     // Remove the event listener after the button is clicked
-    event.currentTarget.removeEventListener('click', buttonClickHandler);
+    event.currentTarget.removeEventListener("click", buttonClickHandler);
 
     let selectedSeats = seatContainer.childNodes;
-    let selectedSeatCount = []; 
+    let selectedSeatCount = [];
     for (let selectedSeat of selectedSeats) {
-        selectedSeatCount.push(selectedSeat.innerText);
+      selectedSeatCount.push(selectedSeat.innerText);
     }
-  
-   
-     
-   
-    const grandTotalPrice = document.getElementById('grand-total');
+
+    const grandTotalPrice = document.getElementById("grand-total");
     grandTotalPrice.innerText = grandTotal;
-     }
-     
-     else{
-        alert('you cannot buy more then 4 tickets')
-     }
-       
+  } else {
+    alert("you cannot buy more then 4 tickets");
+  }
 }
 
-const btn = document.getElementById('apply-btn');
-btn.addEventListener('click', function(){
-    let couponElement = document.getElementById('input-field').value;
-    const couponCode = couponElement.split(' ').join('').toUpperCase();
-    console.log(couponCode);
+const btn = document.getElementById("apply-btn");
+btn.addEventListener("click", function () {
+  let couponElement = document.getElementById("input-field").value;
+  const couponCode = couponElement.split(" ").join("").toUpperCase();
+  console.log(couponCode);
 
-    if(couponCode === "NEW15"){
-        if(selectedSeatNo === 4){
-            
-           let discountPrice = totalTicketPrice * 0.15;
-            grandTotal -= discountPrice;
-            const discountContainer = document.getElementById('discount-container');
-            const p = document.createElement('p');
-            p.innerText = "discount Price :" +  discountPrice + "Tk Only";
-            discountContainer.appendChild(p);
-          
-           
-            
-
-        }else{
-            alert('You have to purchase 4 tickets to get the discount')
-        }
-    } else if (couponCode === "COUPLE20") {
-        let discountPrice = totalTicketPrice * 0.20;
-        grandTotal -= discountPrice; 
-
-        const discountContainer = document.getElementById('discount-container');
-         const p = document.createElement('p');
-         const p2 = document.createElement('p');
-         p.innerText = "discount Price : " + discountPrice + " Tk Only" ;
-         
-         discountContainer.appendChild(p);
-         
-         
+  if (couponCode === "NEW15") {
+    if (selectedSeatNo === 4) {
+      let discountPrice = totalTicketPrice * 0.15;
+      grandTotal -= discountPrice;
+      const discountContainer = document.getElementById("discount-container");
+      const p = document.createElement("p");
+      p.innerText = "discount Price :" + discountPrice + "Tk Only";
+      discountContainer.appendChild(p);
+    } else {
+      alert("You have to purchase 4 tickets to get the discount");
     }
-    
-    else{
-        alert('Invalid couponCode')
-    }
+  } else if (couponCode === "COUPLE20") {
+    let discountPrice = totalTicketPrice * 0.2;
+    grandTotal -= discountPrice;
 
-    const restTotal = document.getElementById('grand-total');
-    restTotal.innerText = grandTotal;
-    document.getElementById('input-field').value = "";
+    const discountContainer = document.getElementById("discount-container");
+    const p = document.createElement("p");
+    const p2 = document.createElement("p");
+    p.innerText = "discount Price : " + discountPrice + " Tk Only";
 
-    
-     
-})
+    discountContainer.appendChild(p);
+  } else {
+    alert("Invalid couponCode");
+  }
 
-
-
-
-
-
-// Attach the event listener to each button
-const buttons = document.querySelectorAll('.myButton');
-buttons.forEach(button => {
-    button.addEventListener('click', buttonClickHandler);
+  const restTotal = document.getElementById("grand-total");
+  restTotal.innerText = grandTotal;
+  document.getElementById("input-field").value = "";
 });
 
-
-
-
+// Attach the event listener to each button
+const buttons = document.querySelectorAll(".myButton");
+buttons.forEach((button) => {
+  button.addEventListener("click", buttonClickHandler);
+});
