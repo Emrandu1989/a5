d49f741e42b9e9d80c7c090932f00f1553daf438
Emrandu1989 <emrandu1989@gmail.com>
2024-02-18 18:26:47 +0600

    discount price added

--- js/index.js
+++ js/index.js
@@ -1,56 +1,14 @@
 
-
-// let len = document.querySelectorAll(".myButton").length;
-// console.log(len);
-
-// let selectedSeatCount = []
-
-
-/* for( let i = 0; i< len; i++){
-    document.querySelectorAll('.myButton')[i].addEventListener('click', function(){
-        console.log('btn-clicked')
-        let text = this.innerText;
-         
-        
-
-        
-        const seatContainer = document.getElementById('seat-no');
-      
-        const p = document.createElement('p');
-        p.innerText = text;
-        seatContainer.appendChild(p);
-
-        this.style.backgroundColor = '#1DD100';
-        // console.log(seatContainer.childNodes)
-        let selectedSeats = seatContainer.childNodes;
-         for(let selectedSeat of selectedSeats){
-            selectedSeatCount.push(selectedSeat.innerText);
-         }
-
-        
-       
-         console.log(selectedSeatCount.length);
-         if(selectedSeatCount.length > 14){
-            alert("you cannot buy more then 4 ticket")
-         }
-            
-         function buttonClickHandler() {
-            // Event handling logic
-            this.removeEventListener('click', buttonClickHandler);
-        }
-        
-      
-    })
-}
- */
-
 let selectedSeatNo = 0;
 let seatLeftCount = 40;
 let totalTicketPrice = 0;
+let grandTotal = 0;
+let ticketLimit = 4;
 
 
 function buttonClickHandler(event) {
-    console.log('btn-clicked');
+     if(selectedSeatNo < ticketLimit){
+        console.log('btn-clicked');
     let text = this.innerText;
     
     const seatContainer = document.getElementById('seat-no');
@@ -61,12 +19,9 @@ function buttonClickHandler(event) {
     this.style.backgroundColor = '#1DD100';
     selectedSeatNo++;
     seatLeftCount--; 
-    totalTicketPrice += 500;
-    // if(selectedSeatNo> 4){
-    //      alert('You cannot buy more than 4 tickets.');
-        
-
-    // }
+    totalTicketPrice += 550;
+    grandTotal +=550;
+   
      
     const seatCount = document.getElementById('selectedSeat-no');
     seatCount.innerText = selectedSeatNo;
@@ -76,21 +31,58 @@ function buttonClickHandler(event) {
     const totalPrice = document.getElementById('total-price');
     totalPrice.innerText = totalTicketPrice;
 
+   
+
     // Remove the event listener after the button is clicked
     event.currentTarget.removeEventListener('click', buttonClickHandler);
 
     let selectedSeats = seatContainer.childNodes;
-    let selectedSeatCount = []; // Define selectedSeatCount array here
+    let selectedSeatCount = []; 
     for (let selectedSeat of selectedSeats) {
         selectedSeatCount.push(selectedSeat.innerText);
     }
-
-    console.log(selectedSeatCount.length);
-    // if (selectedSeatCount.length > 14) {
-    //     alert("You cannot buy more than 4 tickets.");
-    
-    // }
+  
+   
+     
+   
+    const grandTotalPrice = document.getElementById('grand-total');
+    grandTotalPrice.innerText = grandTotal;
+     }
+     
+     else{
+        alert('you cannot buy more then 4 tickets')
+     }
+       
 }
+
+const btn = document.getElementById('apply-btn');
+btn.addEventListener('click', function(){
+    const couponElement = document.getElementById('input-field').value;
+    const couponCode = couponElement.split(' ').join('').toUpperCase();
+    console.log(couponCode);
+
+    if(couponCode === "NEW15"){
+        if(selectedSeatNo === 4){
+            
+           let discountPrice = totalTicketPrice * 0.15;
+            grandTotal -= discountPrice;
+          
+            const restTotal = document.getElementById('grand-total');
+            restTotal.innerText = grandTotal;
+
+        }else{
+            alert('You have to purchase 4 tickets to get the discount')
+        }
+    }else{
+        alert('Invalid couponCode')
+    }
+     
+})
+
+
+
+
+
 
 // Attach the event listener to each button
 const buttons = document.querySelectorAll('.myButton');
@@ -98,13 +90,6 @@ buttons.forEach(button => {
     button.addEventListener('click', buttonClickHandler);
 });
 
-// Function to disable all buttons
-function disableAllButtons() {
-    const buttons = document.querySelectorAll('.myButton');
-    buttons.forEach(button => {
-        button.disabled = true;
-    });
-}
 
 
 
